799eb527a7f6c08dd403b60891688fc0db9f7f71
Hemant Makkar <gautam.makkar2001@gmail.com>
2020-05-14 18:46:16 +0530

    Initial Commit

---
+++ 466A - Cheap Travel.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n{0}, m{0}, a{0}, b{0};
+    cin >> n >> m >> a >> b;
+    int x{0}, y{0};
+    int price{0};
+    if(b/m < a){
+        x=n/m;
+        y=n%m;
+        if(y==0)
+            price+=x*b;
+        else if(m>n)
+            price = min(n*a, b);
+        else
+            price = (a>b) ? x*b+b : x*b+y*a;
+    }
+    else 
+        price=n*a;
+        
+    cout << price << endl; 
+}
